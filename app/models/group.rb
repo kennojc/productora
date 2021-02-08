37e7cfa557ad5bd1d709041218dc294c60860c24
Kenneth Johnson C <kennojc@gmail.com>
2021-02-08 18:22:52 -0300

    Added methods in Group class for Group index

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,6 +3,15 @@ class Group < ApplicationRecord
 
     enum bandtype: [:male, :female, :band]
 
+    def total_concerts
+        concerts.includes(group_id: id).count
+    end
 
+
+    def attendance
+        concerts.includes(group_id: id).each {|c| c.tickets}.sum
+    end
+
+    
 
 end
